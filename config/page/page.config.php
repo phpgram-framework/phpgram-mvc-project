c51d74f43038a1f501fadaa5d896a7d40586b5fd
Jörn Heinemann <joernheinemann@gmx.de>
2019-11-24 11:56:19 +0100

    update auf neue version

--- config/page/page.config.php
+++ config/page/page.config.php
@@ -2,11 +2,3 @@
 /**
  * Weitere Seiteneinstellungen die hier individuel angepasst werden können
  */
-
-//Userdaten
-define('VIDEOBACK', STORAGE."video".DIRECTORY_SEPARATOR);
-define("VIDEOFRONT",DIRECTORY_SEPARATOR."storage".DIRECTORY_SEPARATOR."video".DIRECTORY_SEPARATOR);
-define("IMGBACK",STORAGE."img".DIRECTORY_SEPARATOR);
-define("IMGFRONT",DIRECTORY_SEPARATOR."storage".DIRECTORY_SEPARATOR."img".DIRECTORY_SEPARATOR);
-
-//Eigene Einstellungen____________________________________________________
